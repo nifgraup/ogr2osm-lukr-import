4e9d0951a517a7ff85afec18025b4d3461caae18
Björgvin Ragnarsson <nifgraup@gmail.com>
2010-11-15 23:04:03 +0000

    lukr.py fixed

--- translations/lukr.py
+++ translations/lukr.py
@@ -1,22 +1,30 @@
 """
 Translation rules for data from LUKR, the Reykjavik GIS department.
+
+Usage:
+
+./ogr2osm.py -t lukr ../lukr-osm-donation-2010-09-18/gonguleidir_LUKR_170910.shp
+
+Original attributes:
+
+['OBJECTID', 'STIGAFLOKK', 'BREIDD', 'TEIKNINR', 'ASTAND', 'AR_LAGT', 'AR_LAGF', 'TEG', 'FLOKKUR', 'RUTT', 'AR_ADG', 'UTBSV', 'RUTT_BREID', 'DAGS_INN', 'NOTANDI', 'DAGS_BREYT', 'BREYTANDI', 'SVF', 'UPPR', 'NAKV', 'DAGS_UPPR', 'DAGS_LEIDR', 'DAGS_UPPF', 'GAGNA_EIGN', 'HEIMILD', 'NAFN_G', 'NAKV_XY', 'VIDMIDUN_P', 'VINNSLA_F', 'SHAPE_LEN']
+
+Extra attributes can be added later if needed, as OBJECTID is unique.
 """
-
-
 
 def translateAttributes(attrs):
 	if not attrs: return
 	
 	tags = {}
 	
-	if attrs['BREIDD']:
-		tags = {'width':attrs['BREIDD']}
+	if attrs['BREIDD'] != '  0.00':
+		tags = {'width':attrs['BREIDD'].lstrip()}
 	
 	if attrs['NAKV']:
-		tags = {'PRECISION':attrs['NAKV']}
+		tags.update({'PRECISION':attrs['NAKV'].lstrip()})
 	
 	if attrs['OBJECTID']:
-		tags = {'lukr:objectid':attrs['OBJECTID']}
+		tags.update({'lukr:objectid':attrs['OBJECTID']})
 	
 	#Todo: Stigaflokkur
 	if attrs['STIGAFLOKK'] == '1':#Adalstigur
@@ -29,11 +37,11 @@ def translateAttributes(attrs):
 		tags.update({'lukr:highway':'construction','construction':'road'})
 	
 	elif attrs['STIGAFLOKK'] == '4':#Malarstigur
-		tags.update({'lukr:highway':'service'})
+		tags.update({'surface':'unpaved'})
 	
 	#Todo: Tegund
 	if attrs['TEG'] == '1':#Stigur
-		tags.update({'lukr:highway':'road'})
+		tags.update({'lukr:highway':'path'})
 		
 	elif attrs['TEG'] == '2':#Stett
 		tags.update({'lukr:highway':'trunk'})
@@ -42,7 +50,7 @@ def translateAttributes(attrs):
 		tags.update({'lukr:highway':'construction','construction':'road'})
 	
 	elif attrs['TEG'] == '4':#Malarstigur
-		tags.update({'lukr:highway':'service'})
+		tags.update({'surface':'unpaved'})
 	
 	return tags
 

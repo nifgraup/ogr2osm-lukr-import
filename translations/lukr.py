04f9cd919c382c2301045f22f70a9a3a9c5bd8d5
Björgvin Ragnarsson <nifgraup@gmail.com>
2014-07-28 01:16:07 +0300

    change lukr import translator for ocnverting bicycle parking data

--- translations/lukr.py
+++ translations/lukr.py
@@ -1,15 +1,10 @@
 """
-Translation rules for data from LUKR, the Reykjavik GIS department.
+Translation rules for bycycle parking data from LUKR, the Reykjavik GIS department.
 
 Usage:
 
-./ogr2osm.py -t lukr ../lukr-osm-donation-2010-09-18/gonguleidir_LUKR_170910.shp
+./ogr2osm.py -t lukr ../Hjolraeidastaedi_HBsv_shapefile_20140701/hjolreidastaedi.shp
 
-Original attributes:
-
-['OBJECTID', 'STIGAFLOKK', 'BREIDD', 'TEIKNINR', 'ASTAND', 'AR_LAGT', 'AR_LAGF', 'TEG', 'FLOKKUR', 'RUTT', 'AR_ADG', 'UTBSV', 'RUTT_BREID', 'DAGS_INN', 'NOTANDI', 'DAGS_BREYT', 'BREYTANDI', 'SVF', 'UPPR', 'NAKV', 'DAGS_UPPR', 'DAGS_LEIDR', 'DAGS_UPPF', 'GAGNA_EIGN', 'HEIMILD', 'NAFN_G', 'NAKV_XY', 'VIDMIDUN_P', 'VINNSLA_F', 'SHAPE_LEN']
-
-Extra attributes can be added later if needed, as OBJECTID is unique.
 """
 
 def translateAttributes(attrs):
@@ -19,44 +14,17 @@ def translateAttributes(attrs):
 
 # Include all original tags with the lukr: prefix
 	for k, v in attrs.iteritems():
-		tags.update({'lukr:'+k:v})
-#	tags.update({'lukr:raw':str(attrs)}) #all lukr original tags, including the unique ObjectId.
+		tags.update({'ref:hjolastaedi:'+k:v})
 	
-	#tags on all paths
-	tags.update({'source':'lukr', 'source:date':'2010-09-17'})
-	tags.update({'lukr:highway':'footway'}) #remove the lukr: prefix when the path has been reviewed.
-	tags.update({'bicycle':'yes'}) #bicycles are allowed on all roads
+	# tags for all bike parking nodes
+	tags.update({'source':'lukr', 'source:date':'2014-06-30'})
+	tags.update({'amenity':'bicycle_parking'})
 	
-	#add width if it's not zero
-	if attrs['BREIDD'] != '  0.00':
-		tags.update({'width':attrs['BREIDD'].lstrip()})
-	
-#Stigaflokkur see http://www.gamli.umhverfissvid.is/Files/Skra_0014564.pdf
-#This classification system for pathways is for administrative use.
-	if attrs['STIGAFLOKK'] == '4':#Malarstigur / gravel path
-		tags.update({'surface':'gravel'})
-#	elif attrs['STIGAFLOKK'] == '1':#Adalstigur / main path
-#		tags.update({'lukr:highway':'path'}) # already added	
-#	elif attrs['STIGAFLOKK'] == '2':#Tengistigur / secondary path, http://www.althingi.is/altext/125/s/0760.html
-#		tags.update({'lukr:highway':'tengistigur'})
-#	elif attrs['STIGAFLOKK'] == '3':#Adrir stigar
-#		tags.update({'lukr:highway':'unclassified'})
-	
-#Tegund
-	if attrs['TEG'] == '2':#Stett
-		tags.update({'surface':'paved'})
-	elif attrs['TEG'] == '3':#Hitaveitustigur
-		tags.update({'man_made':'pipeline','location':'overground','type':'hot_water'})
-	elif attrs['TEG'] == '4':#Malarstigur
-		tags.update({'surface':'gravel'})
-#	elif attrs['TEG'] == '1':#Stigur
-#		tags.update({'lukr:highway':'path'}) # already added
-	
-#Flokkur
-	if attrs['FLOKKUR'] == '217':#midlinur stettar
-		tags.update({'surface':'paved'})
-#	elif attrs['FLOKKUR'] == '215':#midlinur stigs
-#		tags.update({'highway':'path'})
+	# type of bicycle parking
+	if attrs['Tegund'] == 'bogar':
+		tags.update({'bicycle_parking':'wall_loops'})
+	else: #TODO: the rest
+		tags.update({'bicycle_parking':'stands'})
 	
 	return tags
 
